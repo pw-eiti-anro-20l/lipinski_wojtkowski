c78c7810a375a6b414010525058c97bacde947c8
maron20 <marek.t.l@wp.pl>
2020-04-06 08:47:05 +0200

    Final fixes+ static launch

--- LAB_2/src/dh2rpy.cpp
+++ LAB_2/src/dh2rpy.cpp
@@ -22,7 +22,7 @@ int main( int argc, char** argv) {
 	}
 	double dh_params[3][4] = {{0, 0, 0, arg_map["theta1"]},
 	                           {arg_map["a1"], 0, 0, arg_map["theta2"]},
-							   {arg_map["a2"], arg_map["alpha1"], arg_map["d3"], 0}};
+							   {arg_map["a2"], arg_map["alpha2"], arg_map["d3"], 0}};
 	
 	std::ofstream  config;
 	config.open("../../../src/lipinski-wojtkowski/LAB_2/config/config.yaml", std::ofstream::out | std::ofstream::trunc);
@@ -35,10 +35,12 @@ int main( int argc, char** argv) {
 	config << "a2: " << arg_map["a2"] << "\n";
 	config << "d3: " << arg_map["d3"] << "\n";
 	for(int i = 0; i < 3; ++i) {
-		KDL::Frame frame = KDL::Frame::DH(dh_params[i][0], dh_params[i][1], dh_params[i][2], dh_params[i][3]);
+		KDL::Frame frame = KDL::Frame::DH_Craig1989(dh_params[i][0], dh_params[i][1], dh_params[i][2], dh_params[i][3]);
 		double roll, pitch, yaw;
 		frame.M.GetRPY(roll, pitch, yaw);
-		config << "joint" << i+1 << "-rpy: \"" << roll << " " << pitch << " " << yaw << "\"\n";
+		config << "joint" << i+1 << ": {\n";
+		config << " rpy: \"" << roll << " " << pitch << " " << yaw << "\"," << "\n";
+		config << " pos: \"" << frame.p.x() << " " << frame.p.y() << " " << frame.p.z() << "\",\n}\n";
 	}
 	config << "...";
 	config.close();
@@ -55,7 +57,7 @@ std::pair<std::string, double> getArg(std::string str) {
 }
 
 bool check_arg_map(const std::map<std::string, double> & arg_map) {
-	std::string keys[] = {"a1", "a2", "d3", "alpha1", "theta1", "theta2"};
+	std::string keys[] = {"a1", "a2", "d3", "alpha2", "theta1", "theta2"};
 	for(int i = 0; i < 6; ++i) {
 		if(arg_map.find(keys[i]) == arg_map.end())
 			return false;
