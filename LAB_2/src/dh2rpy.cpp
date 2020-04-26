17293d1734e864c0d209a6b892b9ab319e060bd2
maron20 <marek.t.l@wp.pl>
2020-04-26 20:00:03 +0200

    KDL Final reworks

--- LAB_2/src/dh2rpy.cpp
+++ LAB_2/src/dh2rpy.cpp
@@ -7,27 +7,39 @@
 #include <map>
 #include <utility>
 #include <stdexcept>
+#include <cctype>
 
-std::pair<std::string, double> getArg(std::string str);
-bool check_arg_map(const std::map<std::string, double> & map);
+using namespace std;
+
+pair<string, double> getArg(string str);
+string get_dir(string str);
+pair<string, string> split_arg(string str);
+bool check_arg_map(const map<string, double> & map);
 
 int main( int argc, char** argv) {
-	std::map<std::string, double> arg_map;
+	map<string, double> arg_map;
+	string config_dir;
 	for(int i = 1; i < argc - 2; ++i) {
-		arg_map.insert(getArg(argv[i]));
+		pair<string, string> arg = split_arg(argv[i]);
+		if(isdigit(arg.second[0]))
+			arg_map.insert(make_pair(arg.first, stod(arg.second)));
+		else if(arg.first == "dir")
+			config_dir = "../../../src/lipinski-wojtkowski/" + arg.second + "/config/config.yaml";
+		else
+			throw invalid_argument("No directory");
 	}
 	if(!check_arg_map(arg_map)) {
-		std::cerr << "Incorrect or missing arguments!\n";
+		cerr << "Incorrect or missing arguments!\n";
 		return -1;
 	}
 	double dh_params[3][4] = {{0, 0, 0, arg_map["theta1"]},
 	                           {arg_map["a1"], 0, 0, arg_map["theta2"]},
 							   {arg_map["a2"], arg_map["alpha2"], arg_map["d3"], 0}};
 	
-	std::ofstream  config;
-	config.open("../../../src/lipinski-wojtkowski/LAB_2/config/config.yaml", std::ofstream::out | std::ofstream::trunc);
+	ofstream  config;
+	config.open(config_dir, ofstream::out | ofstream::trunc);
 	if(!config.is_open()) {
-		std::cerr <<  "Couldn't open config.yaml file\n";
+		cerr <<  "Couldn't open config.yaml file\n";
 		return -1;
 	}
 	config <<"---\n";
@@ -50,17 +62,15 @@ int main( int argc, char** argv) {
 	return 0;
 }
 
-std::pair<std::string, double> getArg(std::string str) {
+pair<string, string> split_arg(string str) {
 	size_t split_point = str.find_first_of('=');
-	if(str[0] != '-' || split_point == std::string::npos)
-		throw std::invalid_argument(str);
-	double value = stod(str.substr(split_point + 1));
-	std::string name = str.substr(1, split_point - 1);
-	return std::make_pair(name, value);
+	if(str[0] != '-' || split_point == string::npos)
+		throw invalid_argument(str);
+	return make_pair(str.substr(1, split_point - 1), str.substr(split_point + 1));
 }
 
-bool check_arg_map(const std::map<std::string, double> & arg_map) {
-	std::string keys[] = {"a1", "a2", "d3", "alpha2", "theta1", "theta2"};
+bool check_arg_map(const map<string, double> & arg_map) {
+	string keys[] = {"a1", "a2", "d3", "alpha2", "theta1", "theta2"};
 	for(int i = 0; i < 6; ++i) {
 		if(arg_map.find(keys[i]) == arg_map.end())
 			return false;
--- lab_3/src/SubscribePublish.h
+++ lab_3/src/SubscribePublish.h
@@ -17,20 +17,13 @@ private:
 	ros::Publisher _pub2;
 	ros::Publisher _pub3;
 	
-	class Robot {
-	public:
+	struct Robot {
 		double a1;
 		double a2;
 		double alpha2;
 		double d3;
 		double theta1;
 		double theta2;
-		
-		Robot() {}
-		Robot(double params[]) : a1(params[0]), a2(params[1]), alpha2(params[2]), d3(params[3]), theta1(params[4]), theta2(params[5]) {}
-		KDL::Frame transform_joint1(double dtheta1);
-		KDL::Frame transform_joint2(double dtheta2);
-		KDL::Frame transform_joint3(double dd3);
 	} _robot;
 	
 	void _publish(const ros::Publisher& pub, const KDL::Frame& pos);
