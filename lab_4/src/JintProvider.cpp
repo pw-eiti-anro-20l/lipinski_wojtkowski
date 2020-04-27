99d7509ccc68c869b89415abf06823fa393ebb5d
maron20 <marek.t.l@wp.pl>
2020-04-27 13:14:30 +0200

    JintProvider created

---
+++ lab_4/src/JintProvider.cpp
@@ -0,0 +1,88 @@
+#include "JintProvider."
+#include <sensor_msgs/JointState.h>
+#include <vector>
+
+using namespace std;
+
+JintProvider::JintProvider() {
+	_server = _nh.advertiseService("Jint_srv", &JintProvider::callback, this);
+	_pub = _nh.advertise<sensor_msgs::JointState>("joint_states", 1000);
+	_position = {0, 0, 0};
+}
+
+bool JintProvider::callback(lab_4::jint_control_srv::Request& req, lab4::jint_control_srv::Response& resp) {
+	vector<double> final_joint_states = req.joint_states;
+	ros::Duration mov_duration = req.mov_duration;
+	string int_type = req.int_type;
+	if(int_type == "lin")
+		iType = Interpolation::LIN;
+	else if(int_type == "cube")
+		iType = Interpolation::CUBE;
+	else {
+		resp->status=false;
+		ROS_ERROR("Interpolation \"" + int_type "\" not supportd"); 
+		return false;
+	}
+	
+	if(mov_duration.toSec() <= 0) {
+		resp->status=false;
+		ROS_ERROR("Duration less or equal 0")
+		return false;
+	}
+	
+	ros::Time start = ros::Time::now();
+	vector<vector<double>> interpol_params
+	for(int i = 0; i < 3; ++i) {
+		vector<double> params;
+		switch(iType) {
+		case Interpolation::LIN:
+			double v = (final_joint_states[i] - _position[i])/mov_duration.toSec();
+			params.push_back(_position[i] - v * start);
+			params.push_back(v);
+			break;
+		case Interpolation::CUBE:
+			calc_cube_params(final_joint_states[i], _position[i], mov_duration.toSec(), start.toSec(), vect);
+			break;
+		}
+		interpol_params.append(params);
+	}
+	
+	while(ros::Time::now() <= start + mov_duration) {
+		vector<double> new_pos;
+		double t = ros::Time::now().toSec();
+		for(int i = 0; i < 3; ++i)
+			new_pos.push_back(interpolate(interpol_params[i], t);
+		_position = new_pos;
+		_publish(_position);
+	}
+	resp->status = true;
+	return true;
+}
+
+void JintProvider::_publish(const vector<double>& position) {
+	sensor_msgs::JointState msg;
+	msg.position = position;
+	msg.name = {"joint1", "joint2", "joint3"};
+	msg.header.stamp = ros::Time::now();
+	msg.header.frame_id = "base";
+	_pub.publish(msg);
+}
+
+void JintProvider::calc_cube_params(double x1, double x0, double dt, double t0, vector<double>& vect) {
+	double tq = t0 + dt;
+	double a0 = (x0*t1-t0*x1)/dt-t1*t0*(t1+t0)*(x1-x0)/pow(dt, 3);
+	double a1 = (x1-x0)/dt + (x1-x0)/pow(dt, 3)*(t1*t1+4*t1*t0+t0*t0);
+	double a2 = 3*(x1-x0)*(t1+t0)/pow(dt, 3);
+	double a3 = 2*(x1-x0)/pow(dt, 3);
+	vect.push_back(a0);
+	vect.push_back(a1);
+	vect.push_back(a2);
+	vect.push_back(a3);
+}
+
+double JintProvider::interpolate(const vector<double>& params, double t) {
+	double sum = 0;
+	for(int i = 0; i < params.size(); ++i)
+		sum += params[i] * pow(t, i);
+	return sum;
+}
--- lab_4/src/jint.cpp
+++ lab_4/src/jint.cpp
@@ -1,2 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <lab4/jint_control_srv.h>
+
+void main(int argc, char** argv) {
+	
+}
